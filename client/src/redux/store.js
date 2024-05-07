2e9112d397eb1dd4e997b321594953840b77a1ac
Kyrylo <fresh.js.ftd.@gmail.com>
2024-05-07 18:23:03 +0300

    Add redux toolkit and new store example

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,19 +1,25 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import createSagaMiddleware from 'redux-saga';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
-import rootSaga from './sagas';
+
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from '@redux-devtools/extension';
+// import createSagaMiddleware from 'redux-saga';
+// import rootSaga from './sagas';
 
 // створюємо міддлвер для бібліотеки redux-saga
-const sagaMiddleware = createSagaMiddleware();
+// const sagaMiddleware = createSagaMiddleware();
 
 // тут ви кажете які саме міддлвери будуть до тсори під'єднані
-const enhancer = applyMiddleware(sagaMiddleware);
+// const enhancer = applyMiddleware(sagaMiddleware);
 
 // треба створити стору (сховище) для стану редакса
 // стора вимагає щоб їй передали редюсер для глобального стану
-const store = createStore(rootReducer, composeWithDevTools(enhancer));
+// const store = createStore(rootReducer, composeWithDevTools(enhancer));
 
-sagaMiddleware.run(rootSaga);
+// sagaMiddleware.run(rootSaga);
+
+const store = configureStore({
+  reducer: rootReducer
+});
 
 export default store;
