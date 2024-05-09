0c4cdb0003563379da41a6a73d44c7ba1ca2fdea
Kyrylo <fresh.js.ftd.@gmail.com>
2024-05-09 20:03:24 +0300

    Add very basic socket.io server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { Server } = require('socket.io');
 const app = require('./app');
 const config = require('./configs/server.json');
 
@@ -6,6 +7,22 @@ const server = http.createServer(app);
 
 const host = config.HOST;
 const port = config.PORT;
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+// on - аналог addEventListener
+// 1 агрумент - назва події (стрінга)
+// 2 аргумент - обробник події, приймає своїм параметром об'єкт сокета
+io.on('connection', function (socket) {
+  // socket - екземпляр з'єднання кліента з серваком
+
+  console.log('client connected');
+  console.log(socket);
+});
 
 server.listen(port, host, () => {
   console.log(`server started - ${host}:${port}`);
